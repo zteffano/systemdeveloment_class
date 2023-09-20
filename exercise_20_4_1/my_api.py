cd5516216ea4c98756865e0433ea78e15a88c13f
zteffano <48279765+zteffano@users.noreply.github.com>
2023-09-20 06:49:50 +0200

    Done - lille finish  / refactor og sidste test

--- exercise_20_4_1/my_api.py
+++ exercise_20_4_1/my_api.py
@@ -1,24 +1,46 @@
-"""
-REST API CLASS
+import requests
+import json
+from time import sleep
 
-ROUTES:
+API_URL = "https://rocky-gorge-77460-611a79604e3d.herokuapp.com"
+
+
+class MyAPI:
+    """
+    A class used to represent an API for a REST API
+
+    fields:
+        api_url: str
+        uid: str
+        pwd: str
+        auth_token: str
+        quotes: dict
+        users : dict
+        my_quotes_id: list
+    methods:
+        register() 
+        login() 
+        get_users() - store users in self.users : dict
+        get_quotes() - store quotes in self.quotes : dict
+        get_quote(qid) - getting quote by id
+        post_quote(quote, attribution) 
+        change_quote(qid, quote, attribution) - Changeing quote via. PUT
+        delete_quote(qid) 
+        print_users() - print users dict in nice formatted way
+        print_quotes() - print quotes dict in a nice formatted way
+        get_my_quotes() - stores and returns a list of the quoutes id that you own  
+
+    USED ROUTES:
     /register - POST
     /login - POST
-    /users - GETi
+    /users - GET
     /quotes - GET
     /quote/:qid - GET
     /quote - POST
     /quote/:qid - PUT
     /qoute/:qid - DELETE
 
-"""
-import requests
-import json
-from time import sleep
-API_URL = "https://rocky-gorge-77460-611a79604e3d.herokuapp.com"
-
-
-class MyAPI:
+    """
 
     def __init__(self, API_URL, uid, pwd, auth_token=""):
         self.api_url = API_URL
@@ -28,80 +50,85 @@ class MyAPI:
         self.quotes = {}
         self.users = {}
         self.my_quotes_id = []
-        self.register() #opretter bruger, hvis ikke der kan logges ind med de oplysninger der er givet
-    def register(self):
-    
-            payload = {
-                "uid": self.uid, 
-                "pwd": self.pwd
-                }
-            response = requests.post(f"{self.api_url}/register", json=payload)
-            if response.status_code == 201:
-                print(f"Welcome {self.uid} - New User created Succesfully")
-            elif response.status_code == 500 and "duplicate key" in response.content.decode("utf-8"):
-                print(f"User {self.uid} already exists")
-            else:
-                print("Something went wrong")
-            return response
+        # opretter bruger, hvis ikke der kan logges ind med de oplysninger der er givet
+        self.register()
 
+    def register(self):
+
+        payload = {
+            "uid": self.uid,
+            "pwd": self.pwd
+        }
+        response = requests.post(f"{self.api_url}/register", json=payload)
+        if response.status_code == 201:
+            print(f"Welcome {self.uid} - New User created Succesfully")
+        elif response.status_code == 500 and "duplicate key" in response.content.decode("utf-8"):
+            print(f"User {self.uid} already exists")
+        else:
+            raise Exception(
+                f"Error: {response.status_code} - {response.content.decode('utf-8')}")
+        return response
 
     def login(self):
         # Test login
         payload = {
-            "uid": self.uid, 
+            "uid": self.uid,
             "pwd": self.pwd
-            }
+        }
 
         response = requests.post(f"{self.api_url}/login", json=payload)
         self.auth_token = response.content.decode("utf-8").strip('"')
         print("My auth token: ", self.auth_token)
         return response
-        
-
+    
     def get_users(self):
         response = requests.get(f"{self.api_url}/users")
         self.users = response.json()
         return response
-
 
     def get_quotes(self):
         response = requests.get(f"{self.api_url}/quotes")
         self.quotes = response.json()
         return response
 
-    def get_quote(self,qid):
+    def get_quote(self, qid):
         response = requests.get(f"{self.api_url}/quote/{qid}")
         return response
 
     def post_quote(self, quote, attribution):
         payload = {
-            "quote": quote, 
+            "quote": quote,
             "attribution": attribution
         }
-        auth =  {"Authorization":self.auth_token}
-        response = requests.post(f"{self.api_url}/quote", headers=auth, json=payload)
+        auth = {"Authorization": self.auth_token}
+        response = requests.post(
+            f"{self.api_url}/quote", headers=auth, json=payload)
         return response
 
-
-    def change_quote(self,qid, quote, attribution):
+    def change_quote(self, qid, quote, attribution):
         payload = {
-            "quote": quote, 
+            "quote": quote,
             "attribution": attribution
         }
-        auth =  {"Authorization":self.auth_token}
-        response = requests.put(f"{self.api_url}/quote/{qid}", headers=auth, json=payload)
+        auth = {"Authorization": self.auth_token}
+        response = requests.put(
+            f"{self.api_url}/quote/{qid}", headers=auth, json=payload)
         return response
 
-    def delete_quote(self,qid):
-        auth = {"Authorization":self.auth_token}
+    def delete_quote(self, qid):
+        auth = {"Authorization": self.auth_token}
         response = requests.delete(f"{self.api_url}/quote/{qid}", headers=auth)
         return response
-    
+
     def print_users(self):
-        print(json.dumps(self.users,indent=2))
-    
+        if not self.users:
+            self.get_users()
+        print(json.dumps(self.users, indent=2))
+
     def print_quotes(self):
-        print(json.dumps(self.quotes,indent=2))
+        if not self.quotes:
+            self.get_quotes()
+        print(json.dumps(self.quotes, indent=2))
 
     def get_my_quotes(self):
         self.get_quotes()
@@ -112,10 +139,9 @@ class MyAPI:
                     my_quotes.append(v["qid"])
         self.my_quotes_id = my_quotes
         return self.my_quotes_id
-   
-      
-
- 
 
 
-api = MyAPI(API_URL, "steffan", "1234")
+if __name__ == "__main__":
+    api = MyAPI(API_URL, "steffan", "1234")
+    api.print_users()
+    api.print_quotes()
--- exercise_20_4_1/test_myapi.py
+++ exercise_20_4_1/test_myapi.py
@@ -27,45 +27,68 @@ def test_register():
     assert response.status_code == 201 or (response.status_code == 500 and "duplicate key" in response.content.decode("utf-8"))
 
 def test_login():
-    # Test login
+    # ACT
     response = api.login()
     auth_token = response.content.decode("utf-8")
-
+  
+    # ASSERT
     assert response.status_code == 200
     assert "Bearer" in auth_token
     
 
 def test_users():
-
+    # ACT
     response = api.get_users()
+    
+    # ASSERT
     assert response.status_code == 200
 
 def test_quotes_get():
-
+    # ACT
     response = api.get_quotes()
+
+    # ASSERT
     assert response.status_code == 200
 
 def test_quote_get_id():
+    # ARRANGE
     qid = api.get_my_quotes()[-1]
+    
+    # ACT
     response = api.get_quote(qid)
+
+    # ASSERT
     assert response.status_code == 200
 
 def test_quote_post():
+    # ARRANGE
     quote = "pytest quoteXX"
     attribution = "pytest attributionX"
+    
+    # ACT
     response = api.post_quote(quote=quote, attribution=attribution)
+    
+    # ASSERT
     assert response.status_code == 201
 
-
 def test_quote_put():
+    # ARRANGE
     qid = api.get_my_quotes()[-1]
     quote = "pytest quote"
     attribution = "pytest attribution"
+    
+    # ACT
     response = api.change_quote(qid=qid, quote=quote, attribution=attribution)
+    
+    # ASSERT
     assert response.status_code == 204 # No content
 
 def test_quote_delete():
+    # ARRANGE
     qid = api.get_my_quotes()[-1]
+    
+    # ACT
     response = api.delete_quote(qid)
+    
+    # ASSERT
     assert response.status_code == 204 # No content
-
